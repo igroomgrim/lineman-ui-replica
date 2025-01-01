08d9fe315fcb6ab94331fb83fb06cb68b365ac45
Anak Mirasing <igroomgrim@icloud.com>
2025-01-01 15:21:59 +0700

    🎉 build: initial Xcode project

---
+++ LineManUIRep/LineManUIRepApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineManUIRepApp.swift
+//  LineManUIRep
+//
+//  Created by Anak Mirasing on 1/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct LineManUIRepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
